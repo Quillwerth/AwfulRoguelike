f380a48773b37bd4ef7de48f7b3afadece71d8a1
Jessica McMillan <kemp0158@umn.edu>
2013-06-11 21:41:43 -0500

    growl

--- A_Bad_Game/src/world/Tile.java
+++ A_Bad_Game/src/world/Tile.java
@@ -4,14 +4,45 @@
  */
 package world;
 
+import world.mob.MapObject;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Patrick
  */
 public class Tile {
-   List<MapObject> contents;
+    private List<MapObject> contents;
     
-    public Tile(){
+    public TileType type;
+    
+    public Tile(TileType t){
         contents = new ArrayList<MapObject>();
+        type = t;
+    }
+    
+    /**
+     * Move a MapObject from this tile to another tile.
+     * @param thing the MapObject to be moved
+     * @param other the other tile
+     * @return true on successful move, false otherwise
+     */
+    public boolean moveMobTo(MapObject thing, Tile other){
+        if(contents.contains(thing)){
+            contents.remove(thing);
+            other.enter(thing);
+            thing.updateLocation(other);
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Bam! This MapObject is in the tile now.
+     * @param thing 
+     */
+    private void enter(MapObject thing){
+        contents.add(thing);
     }
 }
---
+++ A_Bad_Game/src/world/util/MapFactory.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package world.util;
+
+import java.util.Random;
+import world.Map;
+import world.Tile;
+import world.TileType;
+
+/**
+ *
+ * @author Patrick
+ */
+public class MapFactory {
+    
+    public Map createBasicMap(int x, int y){
+        Map basic = new Map(x,y);
+        Random r = new Random();
+        for(int xi = 1; xi<x-1; xi++){
+            for(int yi = 1; yi<y-1; yi++){
+                if(r.nextDouble()< 0.3333){
+                    basic.getTile(xi, yi).type = TileType.FLOOR;
+                }
+            }
+        }
+        boolean changesMade = false;
+        
+        do{
+            Map nextIter = new Map(x, y);
+            for(int xi = 1; xi<x-1; xi++){
+                for(int yi = 1; yi<y-1; yi++){
+                    
+                }
+            }
+            
+        }while(changesMade);
+        
+        
+        return basic;
+    }
+    
+    private void CA_ModifyTile(Map m, int x, int y){
+        int open = 5;
+        int close = 2;
+        
+        int openNeighbors = 0;
+        
+        for(int xi = Math.max(1, x-1); xi<Math.min(x-1, x+1); xi++){
+            for(int yi = Math.max(1, y-1); yi<Math.min(y-1, y+1); yi++){
+                if(m.getTile(xi, yi).type == TileType.FLOOR){
+                    //TODO: Stuff.
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
