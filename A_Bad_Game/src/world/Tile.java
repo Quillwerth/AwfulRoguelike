96a0dcd2a5593126dc4763181d5e6b5c4217be57
Patrick Severin <pbseverin@gmail.com>
2013-06-11 20:01:43 -0500

    Jessica is already having issues
    oh boy

---
+++ A_Bad_Game/src/world/Tile.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package world;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Tile {
+    List<MapObject> contents;
+    
+    public Tile(){
+        contents = new ArrayList<MapObject>();
+    }
+}
